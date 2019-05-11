d7e75c82ac70c776f17e53946771c9fcdbe010af
lxx14 <lxx1427@gmail.com>
2019-05-11 14:14:30 +0300

    made ABOUT page+styles

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -3,12 +3,12 @@ import './style.scss';
 
 const About = () => {
   return (
-    <div className='page'>
+    <div className='page about-page'>
       <h1>Hello!</h1>
-      <p>This summary complitely made on React, with usage some tehnologies, such us React-Router and Redux</p>
-      <p>Here is navigation panel...</p>
-      <p>...And here is short info about me</p>
-      <p>I'm eagerly waiting for Your feedback, thank You for reading!</p>
+      <p>This summary complitely made on React, with usage some tehnologies, such us React-Router, Redux, SCSS, HTML 5.</p>
+      <p><i className="fas fa-angle-left"></i> Here is navigation panel...</p>
+      <p id='to-left-side'>...And here is short info about me <i className="fas fa-angle-right"></i></p>
+      <p id='to-center'>I'm eagerly waiting for Your feedback, thank You for reading!</p>
     </div>
   )
 }
