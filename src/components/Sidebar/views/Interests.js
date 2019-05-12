168b0caf736bf929a120744f0724dbbb6b6bcafb
lxx14 <lxx1427@gmail.com>
2019-05-12 18:40:19 +0300

    fixed bugs in adaptability and add info to sidebar

--- src/components/Sidebar/views/Interests.js
+++ src/components/Sidebar/views/Interests.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Interests = () => {
   return(
     <div className='interests'>
-    <h2>Interests</h2>
+    <h2>INTERESTS</h2>
+    <p>MMA</p>
+    <p>MotoGP</p>
+    <p>Crossfit</p>
+    <p>Snowboarding</p>
+    <p>Voleyball</p>
+    <p>Billiards</p>
     </div>
   )
 }
--- src/components/Sidebar/views/Languages.js
+++ src/components/Sidebar/views/Languages.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Languages = () => {
   return(
     <div className='languages'>
-    <h2>Languages</h2>
+    <h2>LANGUAGES</h2>
+    <p>English (pre-intermediate)</p>
+    <p>Russian (native)</p>
+    <p>Ukranian (native)</p>
     </div>
   )
 }
