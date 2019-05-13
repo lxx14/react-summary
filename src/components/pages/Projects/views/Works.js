a02533c4358aaf836686262ea8eef375880435c1
lxx14 <lxx1427@gmail.com>
2019-05-13 11:37:27 +0300

    create works block+styles

--- src/components/pages/Projects/views/Works.js
+++ src/components/pages/Projects/views/Works.js
@@ -4,6 +4,11 @@ const Works = (props) => {
   return (
     <div className='works'>
       <h1>Works</h1>
+      <h3>My works on test hosting:</h3>
+      <ul>
+      <li><i className="fas fa-pen"></i> <a href="https://droksanavexler.000webhostapp.com/" target="blank">Cosmmetology lending page</a></li>
+      <li><i className="fas fa-pen"></i> <a href="https://dia-strizh.000webhostapp.com/" target="blank">Detective agency site</a></li>
+      </ul>
     </div>
   )
 }
