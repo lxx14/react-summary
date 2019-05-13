761a196824e157a976c9ae96de20505f9c101e16
lxx14 <lxx1427@gmail.com>
2019-05-13 12:37:34 +0300

    create previous jobs block

--- src/components/pages/Experiences/JobsList.js
+++ src/components/pages/Experiences/JobsList.js
@@ -3,9 +3,17 @@ import { connect } from 'react-redux';
 
 const JobsList = (props) => {
 
+  const renderJobs = props.jobs.map(item=>
+  <div>
+  <h2>{item.func}</h2>
+  <p className='date'>{item.date}</p>
+  <h4>{item.name}</h4>
+  <p>{item.duties}</p>
+  </div>)
+
   return (
     <div className='jobs-list'>
-      <h2>JobsList</h2>
+      {renderJobs}
     </div>
   )
 }
