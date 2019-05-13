2a7541309e251cf14a5c3a62e2c20a9a7625b7c7
lxx14 <lxx1427@gmail.com>
2019-05-13 13:04:34 +0300

    render jabs from redux and create styles

--- src/components/pages/Experiences/JobsList.js
+++ src/components/pages/Experiences/JobsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const JobsList = (props) => {
-  const renderList = props.jobs.map(item=><div>{item.id}</div>)
+  const renderList = props.jobs.map(item=><div key={item.id}><h2>{item.func}</h2><p className='date'>{item.date}</p><h4>{item.name}</h4><p>{item.duties}</p></div>)
   return (
     <div className='jobs-list'>
     {renderList}
