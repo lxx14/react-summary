1882473fc0eb81635fb1b0c1ff790fd734a722c5
lxx14 <lxx1427@gmail.com>
2019-05-14 15:24:44 +0300

    fixed grammary

--- src/components/pages/Projects/views/Works.js
+++ src/components/pages/Projects/views/Works.js
@@ -6,7 +6,7 @@ const Works = (props) => {
       <h1>Works</h1>
       <h3>My works on test hosting:</h3>
       <ul>
-      <li><i className="fas fa-pen"></i> <a href="https://droksanavexler.000webhostapp.com/" target="blank">Cosmmetology lending page</a></li>
+      <li><i className="fas fa-pen"></i> <a href="https://droksanavexler.000webhostapp.com/" target="blank">Cosmetology landing page</a></li>
       <li><i className="fas fa-pen"></i> <a href="https://dia-strizh.000webhostapp.com/" target="blank">Detective agency site</a></li>
       </ul>
     </div>
