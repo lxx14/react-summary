aff483e179b1d3aa59dde9acccb2077d3b98588c
lxx14 <lxx1427@gmail.com>
2019-05-27 23:58:02 +0300

    change image settings

--- src/components/Sidebar/views/Photo.js
+++ src/components/Sidebar/views/Photo.js
@@ -4,7 +4,7 @@ const Photo = () => {
   return(
     <div className='photo'>
       <div className='photo-wrap'>
-        <img src='https://lh3.googleusercontent.com/u2nPs-me4cVbA0NDGRgYTBC_kaumzskSizP0d4KqUxlblwVysdc1VhIOmgyizto_4auZ=s113' alt='my-photo' />
+        <p>My photo :)</p>
       </div>
       <h3>Ilchenko Alexey</h3>
       <p>Front-End Dev (React)</p>
