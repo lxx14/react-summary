fa19cdb874104638dfecfd817f5883a1fa92ebf1
lxx14 <lxx1427@gmail.com>
2019-07-16 11:11:55 +0300

    add new skills and works

--- src/components/pages/Projects/views/Works.js
+++ src/components/pages/Projects/views/Works.js
@@ -6,8 +6,9 @@ const Works = (props) => {
       <h1>Works</h1>
       <h3>My works on test hosting:</h3>
       <ul>
-      <li><i className="fas fa-pen"></i> <a href="https://droksanavexler.000webhostapp.com/" target="blank">Cosmetology landing page</a></li>
-      <li><i className="fas fa-pen"></i> <a href="https://dia-strizh.000webhostapp.com/" target="blank">Detective agency site</a></li>
+        <li><i className="fas fa-pen"></i> <a href="https://droksanavexler.000webhostapp.com/" target="blank">Cosmetology landing page</a></li>
+        <li><i className="fas fa-pen"></i> <a href="https://dia-strizh.000webhostapp.com/" target="blank">Detective agency site</a></li>
+        <li><i className="fas fa-pen"></i> <a href="https://dev.xr.globalvirtual.world/#/auth" target="blank">Global Virtual (commercial project with integration in Play market)</a></li>
       </ul>
     </div>
   )
